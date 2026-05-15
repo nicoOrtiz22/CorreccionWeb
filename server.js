3cda297c78023f8b0e7c8113feadd46dbaff9a7b
nicoOrtiz22 <n.ortiz43@alumnos.santotomas.cl>
2026-05-15 13:32:51 -0400

    tercer commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT =3000;
+const pedidoRoutes = require('./src/routes/pedidoRoutes')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.use('/pedidos', pedidoRoutes)
 app.listen(PORT,()=>{
     console.log("servidor corriendo ")
 })
